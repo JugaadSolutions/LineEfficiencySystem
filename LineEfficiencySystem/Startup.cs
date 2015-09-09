8b0de6c1806db386ddd6bcaaa9243dbb09a3ec36
Ideonics-in <ideonics.in@gmail.com>
2015-09-10 05:28:10 +0530

    Initial Create

---
+++ LineEfficiencySystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LineEfficiencySystem.Startup))]
+namespace LineEfficiencySystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
